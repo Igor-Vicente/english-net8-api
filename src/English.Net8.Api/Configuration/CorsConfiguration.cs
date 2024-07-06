16233c45c6077abdbf27d4bbfc4291357a9553c1
IgorVicente <igor_machado05@hotmail.com>
2024-07-06 17:33:40 -0300

    AddCorsConfiguration configuration

--- src/English.Net8.Api/Configuration/CorsConfiguration.cs
+++ src/English.Net8.Api/Configuration/CorsConfiguration.cs
@@ -2,7 +2,7 @@ namespace English.Net8.Api.Configuration
 {
     public static class CorsConfiguration
     {
-        public static IServiceCollection AddCorsConfiguration(this IServiceCollection services)
+        public static IServiceCollection AddCorsConfiguration(this IServiceCollection services, IConfiguration configuration)
         {
             var authSection = configuration.GetSection("AuthSettings");
             var authSettings = authSection.Get<AuthSettings>();
